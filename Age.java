4976d93ba00ff8a2845bfd58e4c017dfac5d0ca7
SanjaySomnath <sb.sanjay.warangal@gmail.com>
2025-12-10 16:27:24 +0530

    Add files via upload

--- Age.java
+++ Age.java
@@ -1,12 +1,12 @@
 public class Age{
     public static void main(String[] args) {
-        System.out.println("Name- Sanjay");
-        int Telugu= 40;
-        int English= 45;
-        int Hindi= 47;
-        int Maths= 46;
-        int Social_Studies= 49;
-        int Physical_Science= 43;
+        System.out.println("Name- Kriti ");
+        int Telugu= 90;
+        int English= 85;
+        int Hindi= 87;
+        int Maths= 76;
+        int Social_Studies= 89;
+        int Physical_Science= 93;
         int Total= Telugu+English+Hindi+Maths+Social_Studies+ Physical_Science;
         float Marks= Total/ 6;
         System.out.println("Total marks= "+ Total);
